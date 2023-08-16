d9b58b80900bc631705348f805d7a1bde241b554
takna <tak_na@icloud.com>
2023-08-16 16:11:03 +0900

    Exercise 1: Add: footer with Current date and time.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,16 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+  console.log('RSC なので、これは表示されない');
+
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>
+          Page rendered on {``}
+          {new Date().toLocaleString()}
+        </footer>
       </body>
     </html>
   );
